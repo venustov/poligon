3f4f96e6675206f676ffd22130d0067b0b24151e
venustov <venustov@gmail.com>
2020-05-19 22:21:26 +0300

    Созданы сиды и фабрики

--- database/migrations/2020_05_18_210050_create_blog_posts_table.php
+++ database/migrations/2020_05_18_210050_create_blog_posts_table.php
@@ -14,8 +14,30 @@ class CreateBlogPostsTable extends Migration
     public function up()
     {
         Schema::create('blog_posts', function (Blueprint $table) {
+
             $table->bigIncrements('id');
+
+            $table->bigInteger('category_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+
+            $table->string('slug')->unique();
+            $table->string('title');
+
+            $table->text('excerpt')->nullable();
+
+            $table->text('content_raw');
+            $table->text('content_html');
+
+            $table->boolean('is_published')->default(false);
+            $table->timestamp('published_at')->nullable();
+
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('category_id')->references('id')->on('blog_categories');
+            $table->index('is_published');
+
         });
     }
 
