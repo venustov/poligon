adcfbb6b2a01e3027a824294f1067635bda8d550
venustov <venustov@gmail.com>
2020-06-15 21:53:48 +0300

    Laravel урок №40: [ Обновление статьи блога на Ларавел ]

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -6,10 +6,36 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * Class BlogPost
+ * @package App\Models
+ *
+ * @property BlogCategory   $category
+ * @property User           $user
+ * @property string         $title
+ * @property string         $slug
+ * @property string         $content_html
+ * @property string         $content_raw
+ * @property string         $excerpt
+ * @property string         $published_at
+ * @property boolean        $is_published
+ */
 class BlogPost extends Model
 {
 
     use SoftDeletes;
+
+    protected $fillable
+      = [
+        'title',
+        'slug',
+        'category_id',
+        'excerpt',
+        'content_raw',
+        'is_published',
+        'published_at',
+        'user_id',
+      ];
 
     /**
      * Категория статьи
