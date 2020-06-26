267462c3a81eba6b0836a1eea6b3e945eecfd55e
venustov <venustov@gmail.com>
2020-06-26 23:31:05 +0300

    Laravel урок №49: [ Collections. Коллекции #2 ]

--- app/Http/Controllers/DiggingDeeperController.php
+++ app/Http/Controllers/DiggingDeeperController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\BlogPost;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 
 class DiggingDeeperController extends Controller
@@ -35,11 +36,66 @@ class DiggingDeeperController extends Controller
      */
     $collection = collect($eloquentCollection->toArray());
 
-    dd(
-      get_class($eloquentCollection),
-      get_class($collection),
-      $collection
-    );
+//    dd(
+//      get_class($eloquentCollection),
+//      get_class($collection),
+//      $collection
+//    );
+
+//    $result['first'] = $collection->first();
+//    $result['last'] = $collection->last();
+
+    $result['where']['data'] = $collection
+      ->where('category_id', 10)
+      ->values()
+      ->keyBy('id');
+
+//    $result['where']['count'] = $result['where']['data']->count();
+//    $result['where']['isEmpty'] = $result['where']['data']->isEmpty();
+//    $result['where']['isNotEmpty'] = $result['where']['data']->isNotEmpty();
+
+//    // Не очень красиво
+//    if ($result['where']['count']) {
+//      //.......
+//    }
+//
+//    // Так лучше
+//    if ($result['where']['data']->isNotEmpty()) {
+//      //.......
+//    }
+
+//    $result['where_first'] = $collection
+//      ->firstWhere('created_at', '>', '2020-05-20 01:35:11');
+
+//    // Базовая переменная не изменится. Просто вернётся изменённая версия.
+//    $result['map']['all'] = $collection->map(function (array $item) {
+//      $newItem = new \stdClass();
+//      $newItem->item_id = $item['id'];
+//      $newItem->item_name = $item['title'];
+//      $newItem->exists = is_null($item['deleted_at']);
+//
+//      return $newItem;
+//    });
+//
+//    $result['map']['not_exists'] = $result['map']['all']
+//      ->where('exists', '=', false)
+//      ->values()
+//      ->keyBy('item_id');
+//    ;
+
+    // Базовая переменная изменится (трансформируется).
+    $collection->transform(function (array $item) {
+      $newItem = new \stdClass();
+      $newItem->item_id = $item['id'];
+      $newItem->item_name = $item['title'];
+      $newItem->exists = is_null($item['deleted_at']);
+      $newItem->created_at = Carbon::parse($item['created_at']);
+
+      return $newItem;
+    });
+
+    dd($collection);
+
   }
 
   /**
